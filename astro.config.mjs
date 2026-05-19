251786819457997eb928f299052910f1c384318e
Danylo Holubev <genshgame6969@gmail.com>
2026-05-19 17:19:31 +0300

    ui: fix github pages url problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true' && repositoryName.length > 0;
+const isUserOrOrgPagesRepo = repositoryName.endsWith('.github.io');
+
 export default defineConfig({
-  site: 'https://example.com',
+  site: isGitHubPagesBuild ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io` : 'http://localhost:4321',
+  base: isGitHubPagesBuild && !isUserOrOrgPagesRepo ? `/${repositoryName}` : '/',
   output: 'static',
   integrations: [tailwind({ applyBaseStyles: false })],
   vite: {
